2b85f443e363cfac5f63ba6ac63a32d288091f0f
Ly Nguyen <Ly900@aol.com>
2017-10-25 20:21:21 -0400

    Add click and Enter key event listeners.

--- js/app.js
+++ js/app.js
@@ -18,22 +18,34 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UICtrl) {
 
   var $addBtn = $("#add-btn");
+  var $body = $("body");
 
-  $addBtn.on("click", function() {
+  var ctrlAddItem = function() {
 
-    // 1. Get field input data.
-    var userInput = $(".description").val();
+      // 1. Get field input data.
+      var userInput = $(".description").val();
+      test();
+      // 2. Add item to budget controller.
 
-    // 2. Add item to budget controller.
+      // 3. Add item to UI controller.
 
-    // 3. Add item to UI controller.
+      // 4. Calculate the budget.
 
-    // 4. Calculate the budget.
+      // 5. Display the budget in the UI.
 
-    // 5. Display the budget in the UI.
+  }
 
+  $addBtn.on("click", ctrlAddItem);
 
+  $body.on("keypress", function(e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
   });
+
+  function test() {
+    console.log("test");
+  }
 
   return {
 
