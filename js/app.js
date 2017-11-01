9739b00f97945564dc8f3144badc69dd0684abbf
Ly Nguyen <Ly900@aol.com>
2017-11-01 18:10:22 -0400

    put all element selectors into one object and pass from one module to another

--- js/app.js
+++ js/app.js
@@ -1,30 +1,49 @@
 // Budget Controller
 var budgetController = (function() {
 
-  var a = "a";
-
-
 
 })();
 
 // UI Controller
 var UIController = (function() {
+  var DOMstrings = {
+    $inputType: $("select.type"),
+    $inputDescription: $(".description"),
+    $inputValue: $("input.amount"),
+    $addButton: $("#add-btn"),
+    $body: $("body")
+  }
+  function getInput() {
+    return {
+      type: DOMstrings.$inputType.val(),  // Will return "inc" or "exp"
+      description: DOMstrings.$inputDescription.val(),
+      value: DOMstrings.$inputValue.val()
+    }
+  }
 
+  function getDOMstrings() {
+    return DOMstrings;
+  }
 
+  return {
+    getInput: getInput,
+    getDOMstrings: getDOMstrings
+  };
 
 })();
 
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var $addBtn = $("#add-btn");
-  var $body = $("body");
+  var DOM = UICtrl.getDOMstrings();
+
+
 
   var ctrlAddItem = function() {
 
       // 1. Get field input data.
-      var userInput = $(".description").val();
-      test();
+      var userInput = UICtrl.getInput();
+      console.log(UICtrl.getInput());
       // 2. Add item to budget controller.
 
       // 3. Add item to UI controller.
@@ -35,9 +54,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   }
 
-  $addBtn.on("click", ctrlAddItem);
+  DOM.$addButton.on("click", ctrlAddItem);
 
-  $body.on("keypress", function(e) {
+  DOM.$body.on("keypress", function(e) {
     if (e.keyCode === 13 || e.which === 13) {
       ctrlAddItem();
     }
